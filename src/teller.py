cb3157a871f544a945ee9bd0088cda27675e8bb1
brandon-rowe <brandon@cyntax.org>
2019-08-28 21:19:44 -0400

    Update teller.py

--- src/teller.py
+++ src/teller.py
@@ -20,6 +20,7 @@ def login():
     pword = input("Please enter your password. ")
     print()
     credCheck(uname, pword)
+    accountFunc(uname, pword)
 
 def newCustomer():
     uname = input("Please enter a new username. ")
@@ -27,6 +28,7 @@ def newCustomer():
     pword = input("Please enter a new password. ")
     print()
     newCredCheck(uname, pword)
+    accountFunc(uname, pword)
 
 def credCheck(uname, pword):
     #This is where we would have some kind of database check for the credentials.
@@ -40,6 +42,30 @@ def newCredCheck(uname, pword):
     if uname == uname:
         if pword == pword:
             print("Success")
+
+def accountFunc(uname, pword):
+    balance = 0.00
+    print("Hello ", uname)
+    command = input("What would you like to do today? (Deposit(D/d), Withdraw(W/w), or Check Balance(C/c)) ")
+    if command == "D" or "d":
+        depAmt = float(input("How much would you like to deposit? "))
+        balance = deposit(depAmt, balance)
+        print("Your balance after deposit is: ", balance)
+    elif command == "W" or "w":
+        witAmt = float(input("How much would you like to deposit? "))
+        balance = withdraw(witAmt, balance)
+        print("Your balance after withdraw is: ", balance)
+    else:
+        checkBalance(balance)
+
+def withdraw(witAmt, balance):
+    balance -= witAmt
+    return balance
+def deposit(depAmt, balance):
+    balance += depAmt
+    return balance
+def checkBalance(balance):
+    print("Your balance is ", balance)
 
 
 if __name__ == '__main__': main()
